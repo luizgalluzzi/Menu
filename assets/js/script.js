ee5db98502fc03f32bfa5c809df35eb6c93b872b
Luiz galluzzi <luizgalluzzi@yahoo.com.br>
2022-10-25 10:21:48 -0300

    inicial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+
+function menuOnClick () {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+menu-bar.addEventListener ("click", change);
